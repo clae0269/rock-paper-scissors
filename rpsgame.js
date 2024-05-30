4ab0f53b7ea4a5712316dd6562877c8261c9de45
clae0269 <clawish21@gmail.com>
2024-05-30 14:27:39 +0200

    Created both computer choice and created a prompt for human choice

--- rpsgame.js
+++ rpsgame.js
@@ -1,1 +1,20 @@
-function getComputerChoice(rock, paper, scissors);
+function getComputerChoice() {
+    const choices =['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    const validChoices = ['rock', 'paper','scissors'];
+    let userInput = prompt("Please enter your choice(rock, paper or scissors)").toLowerCase();
+
+    while (!validChoices.includes(userInput)) {
+        userInput = prompt("Invalid choice. Please enter rock, paper, or scissors:").toLowerCase();
+    }
+
+    return userInput;
+}
+
+console.log(getHumanChoice());
